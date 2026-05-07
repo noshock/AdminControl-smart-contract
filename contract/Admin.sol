2b6ba097e1f06475d9aa2170d6da3c37bc2bceb0
noshock <72062898+noshock@users.noreply.github.com>
2026-05-07 20:54:06 +0530

    update
    Added functionality for admin control and secret management.

--- contract/Admin.sol
+++ contract/Admin.sol
@@ -1,1 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
 
+contract adminControl{
+
+    uint public updatecount;
+
+    address public  admin;
+
+    string public secret;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "not admin");
+        _;
+
+    }
+
+    event Secretupdated(string newSecret);
+
+
+    function setSecret(string memory _secret) public onlyAdmin {
+        secret = _secret;
+        updatecount++;
+        emit  Secretupdated(_secret);
+    }
+
+    function getSecret() public view returns(string memory) {
+        return secret;
+    }
+    function ChangeAdmin(address newAdmin) public onlyAdmin {
+        admin = newAdmin;
+    }
+
+}
